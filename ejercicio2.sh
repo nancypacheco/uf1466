24b51e5277f2ea047ed5f529e6f253975d99a141
nancypacheco <43095376+nancypacheco@users.noreply.github.com>
2019-01-11 19:45:06 +0100

    Add files via upload
    segundo ejercicio práctico

---
+++ ejercicio2.sh
@@ -0,0 +1,27 @@
+#Unidad Formativa uf1466
+
+ #El crontab nos sirve para  automatizar diversas tareas, es un archivo de texto que guarda una lista 
+ #de comandos a ejecutar en un determinado tiempo, indicado por el usuario y se ejecute en una determinada hora 
+
+
+#crontab * 23 * * *
+
+#con este comando haremos una copia de seguridad de los documentos personales de los usuarios
+rsync -av /home /backups
+#  con este comando haremos una copia de seguridad de los archivos y aplicaciones publicados 
+#en el servidor http
+ rsync -av /var/www/html /backups
+
+#con este comando haremos una copia de seguridad de la base de datos clientes 
+mysqldump -uroot -psecret clientes > clientes.sql
+
+ #con este comando haremos una copia de seguridad del wordpress del servidor Mysql local
+ mysqldump -uroot -psecret wordpress > wordpress.sql
+ 
+ 
+
+
+
+
+
+
